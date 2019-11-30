1cb7d4c25dcbd1fed8bd3102ed6093077101ac4b
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2019-11-29 21:31:28 -0300

    feat: adding menu links in the pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 // import { Container } from './styles';
 
 const AboutPage = () => (
-  <h1>About Page!</h1>
+  <Layout>
+    <SEO title="About"/>
+    <h1>About Page!</h1>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about" activeStyle={{ color: 'red' }}>About (gatsby)</Link>
+      </li>
+    </ul>
+  </Layout>
 );
 
 export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
-import React from "react"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{ color: 'red' }}>Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About (gatsby)</Link>
+      </li>
+    </ul>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
