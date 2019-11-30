cab95860691f40a08aba163d68241acea9999196
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2019-11-29 22:43:53 -0300

    fix: adjusting query

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 // import { Container } from './styles';
 
-const Profile = () => (
-  <StaticQuery
-    query={graphql`
-      query MySiteMetadata {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
+const Profile = () => {
+  const { site: { siteMetadata } } = useStaticQuery(graphql`
+    query MySiteMetadata {
+      site {
+        siteMetadata {
+          title
+          description
+          position
         }
       }
-    `}
-    render={({ site: { siteMetadata } }) => (
-      <div className="Profile-wrapper">
-        <h1>{siteMetadata.title}</h1>
-        <h2>{siteMetadata.position}</h2>
-        <p>{siteMetadata.description}</p>
-      </div>
-    )}
-  />
-);
+    }
+  `);
+
+  return (
+    <div className="Profile-wrapper">
+      <h1>{siteMetadata.title}</h1>
+      <h2>{siteMetadata.position}</h2>
+      <p>{siteMetadata.description}</p>
+    </div>
+  );
+}
 
 export default Profile;
