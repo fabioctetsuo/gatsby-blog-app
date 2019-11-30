a87d6d0f1d1de02435acc8f0232f44a698a320a7
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2019-11-29 23:05:58 -0300

    feat: adding profile photo with gatsby-iamge

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useStaticQuery, graphql } from "gatsby";
+
+import Avatar from '../Avatar';
 // import { Container } from './styles';
 
 const Profile = () => {
@@ -17,6 +19,7 @@ const Profile = () => {
 
   return (
     <div className="Profile-wrapper">
+      <Avatar />
       <h1>{siteMetadata.title}</h1>
       <h2>{siteMetadata.position}</h2>
       <p>{siteMetadata.description}</p>
