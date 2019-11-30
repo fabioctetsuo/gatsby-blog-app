6cfd551d37689be8a8b469a622c05ea0a2c5ff3d
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2019-11-29 21:30:50 -0300

    feat: adding new component Profile in Layout

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Matt Foobar</h1>
+    <h2>Front-end Engineer</h2>
+    <p>Membro da Gympass, escreve sobre o mundo do front-end e outras coisas legais!</p>
+  </div>
+);
+
+export default Profile;
