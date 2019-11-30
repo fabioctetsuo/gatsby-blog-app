ac1605c6b8973a43c1fc771a92caf2d54a06ec04
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2019-11-29 23:09:02 -0300

    feat: using gatsby-image as fluid for test

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -8,8 +8,8 @@ const Avatar = () => {
     query {
       avatarImage: file(relativePath: { eq: "profile-photo.jpeg" }) {
         childImageSharp {
-          fixed(width: 60, height: 60) {
-            ...GatsbyImageSharpFixed
+          fluid(maxWidth: 60, maxHeight: 60) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -17,7 +17,7 @@ const Avatar = () => {
   `);
 
   return (
-    <Img fixed={avatarImage.childImageSharp.fixed} />
+    <Img fluid={avatarImage.childImageSharp.fluid} style={{ width: '60px' }} />
   );
 };
 
