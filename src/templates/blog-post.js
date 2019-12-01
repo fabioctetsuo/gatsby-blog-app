b2f9596530e05fc642733ee9f05678b04913ebdc
Fabio Tetsuo <fabio.tetsuo@gympass.com>
2019-12-01 16:18:39 -0300

    feat: Adding styles to blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,24 +1,44 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
+import Layout from '../components/Layout';
+import SEO from '../components/seo';
+import {
+  PostHeader,
+  PostDate,
+  PostTitle,
+  PostDescription,
+  MainContent,
+} from '../components/Post/styles';
+
 const BlogPost = ({ data }) => {
   const {
     markdownRemark: {
+      timeToRead,
       html,
       frontmatter: {
         title,
         // background,
         // category,
-        // date,
-        // description,
+        date,
+        description,
       },
     }
   } = data;
   return (
-    <>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: html }}></div>
-    </>
+    <Layout>
+      <SEO title={title} />
+        <PostHeader>
+          <PostDate>
+          {date} • {timeToRead} min de leitura
+        </PostDate>
+        <PostTitle>{title}</PostTitle>
+        <PostDescription>{description}</PostDescription>
+      </PostHeader>
+      <MainContent>
+        <div dangerouslySetInnerHTML={{ __html: html }}></div>
+      </MainContent>
+    </Layout>
   );
 };
 
@@ -26,6 +46,7 @@ export const query = graphql`
   query MyQuery($slug: String) {
     markdownRemark(fields: {slug: {eq: $slug}}) {
       html
+      timeToRead
       frontmatter {
         title
         background
